8d40557d552f6fe4235d6cd6bc934b3512f5a6a4
DanielMachado65 <danielmachadopintos@gmail.com>
2018-08-27 11:23:40 -0300

    Quase finalizando o CRUD
    Falta pouco para finalizar o CRUD,  a grande parte de funções já está cadastradas

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,9 @@ app.config['SECRET_KEY'] = 'random-caracteres'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 lg = LoginManager(app)
+lg.login_view = 'login'
+lg.login_message_category = 'info'
+
 
 # importação de rotas.
 from app import routes
